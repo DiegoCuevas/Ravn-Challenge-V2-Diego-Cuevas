87b4a3b7fc0efa0165c78f447a5d86f64cb9c9b7
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-15 18:19:14 -0500

    ADD: scroll to component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import AllPeople from "./table/allPeople"
 function App() {
   return (
-    <div className="h-screen">
-      <div className="bg-black text-white text-base py-3 px-8">Ravn Star Wars Registry</div>
+    <div className="h-screen overflow-y-hidden">
+      <div className="bg-black text-white text-base py-3 px-8">
+        Ravn Star Wars Registry
+      </div>
       <AllPeople></AllPeople>
     </div>
   );
