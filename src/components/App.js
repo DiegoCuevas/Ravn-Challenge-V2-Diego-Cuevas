aec36fac3d58a74c9f80ceec00de72ed62a03620
Diego Cuevas <diegocuevas579@gmail.com>
2021-09-15 13:01:14 -0500

    ADD: components to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
-import '../assets/App.css';
-
+import Table from "./table/table.js"
 function App() {
   return (
-    <div></div>
+    <div className="h-screen">
+      <div className="bg-black text-white text-base py-3 px-8">Ravn Star Wars Registry</div>
+      <Table></Table>
+    </div>
   );
 }
 
